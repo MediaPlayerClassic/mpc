600e7e0eccdaf1f0dcda1a877c1d836d339c97cd
schultz_ <schultz_@627e133f-a60d-0410-aeef-a778af760ea1>
2003-05-31 07:42:46 +0000

    Added GPL Header

--- guliverkli/src/filters/transform/decssfilter/DeCSSFilter.cpp
+++ guliverkli/src/filters/transform/decssfilter/DeCSSFilter.cpp
@@ -1,5 +1,6 @@
 /* 
- *	Copyright (C) Gabest - December 2002
+ *	Copyright (C) 2003 Gabest
+ *	http://www.gabest.org
  *
  *  DeCSSFilter.ax is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -14,6 +15,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with GNU Make; see the file COPYING.  If not, write to
  *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
+ *  http://www.gnu.org/copyleft/gpl.html
  *
  */
 
