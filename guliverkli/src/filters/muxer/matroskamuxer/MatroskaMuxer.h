b6055368f5f6551226274240c7a50cb42c7e665c
gabest <gabest@627e133f-a60d-0410-aeef-a778af760ea1>
2003-06-07 05:09:23 +0000

    *** empty log message ***

---
+++ guliverkli/src/filters/muxer/matroskamuxer/MatroskaMuxer.h
@@ -0,0 +1,125 @@
+/* 
+ *	Copyright (C) 2003 Gabest
+ *	http://www.gabest.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *   
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#pragma once
+
+#include <atlbase.h>
+#include <atlcoll.h>
+#include <afxtempl.h>
+#include "MatroskaFile.h"
+
+#define MAXCLUSTERTIME 5000
+#define MAXBLOCKS 50
+
+class CMatroskaMuxerInputPin : public CBaseInputPin
+{
+	CAutoPtr<MatroskaWriter::TrackEntry> m_pTE;
+
+	bool m_fActive;
+	CCritSec m_csReceive;
+
+	REFERENCE_TIME m_rtLastStart, m_rtLastStop;
+
+public:
+	CMatroskaMuxerInputPin(LPCWSTR pName, CBaseFilter* pFilter, CCritSec* pLock, HRESULT* phr);
+	virtual ~CMatroskaMuxerInputPin();
+
+	DECLARE_IUNKNOWN;
+    STDMETHODIMP NonDelegatingQueryInterface(REFIID riid, void** ppv);
+
+	MatroskaWriter::TrackEntry* GetTrackEntry() {return m_pTE;}
+
+	REFERENCE_TIME m_rtDur;
+
+	CCritSec m_csQueue;
+	CAutoPtrList<MatroskaWriter::BlockGroup> m_blocks;
+	bool m_fEndOfStreamReceived, m_fECCompletSent;
+
+    HRESULT CheckMediaType(const CMediaType* pmt);
+    HRESULT BreakConnect();
+    HRESULT CompleteConnect(IPin* pPin);
+	HRESULT Active(), Inactive();
+
+    STDMETHODIMP NewSegment(REFERENCE_TIME tStart, REFERENCE_TIME tStop, double dRate);
+	STDMETHODIMP BeginFlush();
+	STDMETHODIMP EndFlush();
+
+	STDMETHODIMP Receive(IMediaSample* pSample);
+    STDMETHODIMP EndOfStream();
+};
+
+class CMatroskaMuxerOutputPin : public CBaseOutputPin
+{
+public:
+	CMatroskaMuxerOutputPin(TCHAR* pName, CBaseFilter* pFilter, CCritSec* pLock, HRESULT* phr);
+	virtual ~CMatroskaMuxerOutputPin();
+
+	DECLARE_IUNKNOWN;
+    STDMETHODIMP NonDelegatingQueryInterface(REFIID riid, void** ppv);
+
+    HRESULT DecideBufferSize(IMemAllocator* pAlloc, ALLOCATOR_PROPERTIES* pProperties);
+
+    HRESULT CheckMediaType(const CMediaType* pmt);
+    HRESULT GetMediaType(int iPosition, CMediaType* pmt);
+
+	STDMETHODIMP Notify(IBaseFilter* pSender, Quality q);
+};
+
+[uuid("1E1299A2-9D42-4F12-8791-D79E376F4143")]
+class CMatroskaMuxerFilter 
+	: public CBaseFilter
+	, public CCritSec
+	, public CAMThread
+	, public IAMFilterMiscFlags
+{
+protected:
+	CAutoPtrList<CMatroskaMuxerInputPin> m_pInputs;
+	CAutoPtr<CMatroskaMuxerOutputPin> m_pOutput;
+
+	enum {CMD_EXIT, CMD_RUN};
+	DWORD ThreadProc();
+
+public:
+	CMatroskaMuxerFilter(LPUNKNOWN pUnk, HRESULT* phr);
+	virtual ~CMatroskaMuxerFilter();
+
+#ifdef REGISTER_FILTER
+    static CUnknown* WINAPI CreateInstance(LPUNKNOWN lpunk, HRESULT* phr);
+#endif
+
+	DECLARE_IUNKNOWN;
+    STDMETHODIMP NonDelegatingQueryInterface(REFIID riid, void** ppv);
+
+	void AddInput();
+	UINT GetTrackNumber(CBasePin* pPin);
+
+	int GetPinCount();
+	CBasePin* GetPin(int n);
+
+	STDMETHODIMP Stop();
+	STDMETHODIMP Pause();
+	STDMETHODIMP Run(REFERENCE_TIME tStart);
+
+	// IAMFilterMiscFlags
+
+	STDMETHODIMP_(ULONG) GetMiscFlags();
+};
+
