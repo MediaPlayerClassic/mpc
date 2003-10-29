f2893331b5f8f55e385eac679ee8bfa9c9ecb38d
gabest <gabest@627e133f-a60d-0410-aeef-a778af760ea1>
2003-10-29 21:41:52 +0000

    *** empty log message ***

---
+++ trunk/guliverkli/src/filters/parser/roqsplitter/RoQSplitter.h
@@ -0,0 +1,121 @@
+/* 
+ *	Copyright (C) 2003 Gabest
+ *	http://www.gabest.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *   
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#pragma once
+
+#include "..\BaseSplitter\BaseSplitter.h"
+
+#pragma pack(push, 1)
+struct roq_chunk {WORD id; DWORD size; WORD arg;};
+struct roq_info {WORD w, h, unk1, unk2;};
+#pragma pack(pop)
+
+[uuid("C73DF7C1-21F2-44C7-A430-D35FB9BB298F")]
+class CRoQSplitterFilter : public CBaseSplitterFilter
+{
+	CComPtr<IAsyncReader> m_pAsyncReader;
+
+	REFERENCE_TIME m_rtDuration;
+
+protected:
+	HRESULT CreateOutputs(IAsyncReader* pAsyncReader);
+
+	bool InitDeliverLoop();
+	void SeekDeliverLoop(REFERENCE_TIME rt);
+	void DoDeliverLoop();
+
+public:
+	CRoQSplitterFilter(LPUNKNOWN pUnk, HRESULT* phr);
+
+#ifdef REGISTER_FILTER
+    static CUnknown* WINAPI CreateInstance(LPUNKNOWN lpunk, HRESULT* phr);
+#endif
+
+	// IMediaSeeking
+
+	STDMETHODIMP GetDuration(LONGLONG* pDuration);
+};
+
+[uuid("02B8E5C2-4E1F-45D3-9A8E-B8F1EDE6DE09")]
+class CRoQSourceFilter : public CRoQSplitterFilter
+{
+public:
+	CRoQSourceFilter(LPUNKNOWN pUnk, HRESULT* phr);
+
+#ifdef REGISTER_FILTER
+    static CUnknown* WINAPI CreateInstance(LPUNKNOWN lpunk, HRESULT* phr);
+#endif
+};
+
+
+[uuid("FBEFC5EC-ABA0-4E6C-ACA3-D05FDFEFB853")]
+class CRoQVideoDecoder : public CTransformFilter
+{
+	BYTE* m_y[2];
+	BYTE* m_u[2];
+	BYTE* m_v[2];
+	int m_pitch;
+
+	void Copy(BYTE* pOut, BYTE* pIn, DWORD w, DWORD h);
+
+	#pragma pack(push, 1)
+	struct roq_cell {BYTE y0, y1, y2, y3, u, v;} m_cells[256];
+	struct roq_qcell {roq_cell* idx[4];} m_qcells[256];
+	#pragma pack(pop)
+	void apply_vector_2x2(int x, int y, roq_cell* cell);
+	void apply_vector_4x4(int x, int y, roq_cell* cell);
+	void apply_motion_4x4(int x, int y, unsigned char mv, char mean_x, char mean_y);
+	void apply_motion_8x8(int x, int y, unsigned char mv, char mean_x, char mean_y);
+
+public:
+	CRoQVideoDecoder(LPUNKNOWN lpunk, HRESULT* phr);
+	virtual ~CRoQVideoDecoder();
+
+#ifdef REGISTER_FILTER
+    static CUnknown* WINAPI CreateInstance(LPUNKNOWN lpunk, HRESULT* phr);
+#endif
+	HRESULT Transform(IMediaSample* pIn, IMediaSample* pOut);
+	HRESULT CheckInputType(const CMediaType* mtIn);
+	HRESULT CheckTransform(const CMediaType* mtIn, const CMediaType* mtOut);
+	HRESULT DecideBufferSize(IMemAllocator* pAllocator, ALLOCATOR_PROPERTIES* pProperties);
+	HRESULT GetMediaType(int iPosition, CMediaType* pMediaType);
+
+	HRESULT StartStreaming();
+	HRESULT StopStreaming();
+};
+
+[uuid("226FAF85-E358-4502-8C98-F4224BE76953")]
+class CRoQAudioDecoder : public CTransformFilter
+{
+public:
+	CRoQAudioDecoder(LPUNKNOWN lpunk, HRESULT* phr);
+	virtual ~CRoQAudioDecoder();
+
+#ifdef REGISTER_FILTER
+    static CUnknown* WINAPI CreateInstance(LPUNKNOWN lpunk, HRESULT* phr);
+#endif
+	HRESULT Transform(IMediaSample* pIn, IMediaSample* pOut);
+	HRESULT CheckInputType(const CMediaType* mtIn);
+	HRESULT CheckTransform(const CMediaType* mtIn, const CMediaType* mtOut);
+	HRESULT DecideBufferSize(IMemAllocator* pAllocator, ALLOCATOR_PROPERTIES* pProperties);
+	HRESULT GetMediaType(int iPosition, CMediaType* pMediaType);
+};
+
