60e04f2c49c2274d45dbffe5b6e53c82d153527d
gabest <gabest@627e133f-a60d-0410-aeef-a778af760ea1>
2005-10-01 23:53:40 +0000

    *** empty log message ***

--- trunk/subtitles/include/MySmarty.class.php
+++ trunk/subtitles/include/MySmarty.class.php
@@ -97,21 +97,6 @@ function getParam($name, $i = null)
 	return $ret;
 }
 
-
-function code2utf8($num)
-{
-	if($num < 128) {return chr($num);}
-	if($num < 2048) {return chr(($num >> 6) + 192) . chr(($num & 63) + 128);}
-	if($num < 65536) {return chr(($num >> 12) + 224) . chr((($num >> 6) & 63) + 128) . chr(($num & 63) + 128);}
-	if($num < 2097152) {return chr(($num >> 18) + 240) . chr((($num >> 12) & 63) + 128) . chr((($num >> 6) & 63) + 128) . chr(($num & 63) + 128);}
-	return '';
-}
-
-function html2utf8($str)
-{
-	return preg_replace( '/&#(\\d+);/e', 'code2utf8($1)', utf8_encode($str));
-}
-
 $browser = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'MSIE';
 if(ereg("Opera", $browser)) $browser = "Opera";
 else if(ereg("MSIE", $browser)) $browser = "MSIE";
